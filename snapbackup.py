f606c1f2e1067d349ddda09101972a97c1280c0e
mschlechter <marc@schlechter.nl>
2016-06-11 22:55:34 +0200

    snap backup work

--- snapbackup.py
+++ snapbackup.py
@@ -14,6 +14,11 @@ VOLUME_GROUP="/dev/pluto-vg"
 LOGICAL_VOLUME="root"
 MOUNT_POINT="/mnt/snap"
 
+BACKUP_MOUNT_POINT="/mnt/backup"
+BACKUP_SOURCE="/mnt/snap/home/marc"
+BACKUP_DEST="/mnt/backup"
+BACKUP_NAME="HomeBackup"
+
 # Step 1 : Create the snapshot volume
 args = ["lvcreate", "-L" + SNAPSHOT_SIZE, "-s", "-n", SNAPSHOT_NAME, VOLUME_GROUP + "/" + LOGICAL_VOLUME]
 if subprocess.call(args) != 0:
@@ -30,7 +35,35 @@ if subprocess.call(args) != 0:
 
 print("Snapshot mounted at " + MOUNT_POINT)
 
+# Step 3 : Mount backup location
+if BACKUP_MOUNT_POINT:
+    args = ["mount", BACKUP_MOUNT_POINT]
+    if subprocess.call(args) != 0:
+        print("Mounting the backup location failed.")
+        sys.exit(1)
 
+    print("Backup location mounted at " + BACKUP_MOUNT_POINT)
+    
+# Step 4 : Change to the backup source folder
+os.chdir(BACKUP_SOURCE)
+
+# Step 5 : Create TAR
+tarfile = os.path.join(BACKUP_DEST, BACKUP_NAME + ".tar.gz")
+args = ["tar", "cfz", tarfile, BACKUP_SOURCE]
+if subprocess.call(args) != 0:
+    print("Create TAR file failed.")
+    sys.exit(1)
+
+print("TAR file created")
+
+# Step x : Unmount the backup location
+if BACKUP_MOUNT_POINT:
+    args = ["umount", BACKUP_MOUNT_POINT]
+    if subprocess.call(args) != 0:
+        print("Unmounting the backup location failed.")
+        sys.exit(1)
+
+    print("Backup location mounted at " + BACKUP_MOUNT_POINT)
 
 # Step x : Unmount the snapshot
 args = ["umount", VOLUME_GROUP + "/" + SNAPSHOT_NAME]
