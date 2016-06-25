b4a7175ab1adcb8cb2615824e1ac472de0c0d0fa
mschlechter <marc@schlechter.nl>
2016-06-25 15:50:08 +0200

    more firewall

--- firewall.py
+++ firewall.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
+#
+# Really simple firewall script with support for dynamic ip's
 
 import socket
 
+# Configuration
 
-allowed_hostnames = ["autotaal.mooo.com"]
-allowed_ipaddresses = ["192.168.3.0/24"]
+ALLOWED_HOSTNAMES = ["autotaal.mooo.com"]
+ALLOWED_ADDRESSES = ["192.168.3.0/24"]
+OPEN_PORTS = [22,80,3000]
 
+# End of configuration
 
 all_ips = []
 
-for hostname in allowed_hostnames:
+for hostname in ALLOWED_HOSTNAMES:
     ips = socket.gethostbyname_ex(hostname)[2]
     for ip in ips:
         all_ips.append(ip)
 
-for ip in allowed_ipaddresses:
+for ip in ALLOWED_ADDRESSES:
     all_ips.append(ip)
 
 print (all_ips)
-
 
 # todo : build firewall rules
