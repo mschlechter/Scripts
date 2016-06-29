3e33ae3287f0b72151205755f9c10ac13e128621
Marc Schlechter <marc@schlechter.nl>
2016-06-29 15:54:11 +0200

    homebackup updated

--- homebackup.sh
+++ homebackup.sh
@@ -2,6 +2,7 @@
 
 VOLUME_GROUP="/dev/main-vg"
 LOGICAL_VOLUME="root"
+VOLUME_NAME="$VOLUME_GROUP/$LOGICAL_VOLUME"
 
 SNAPSHOT_NAME="snap"
 SNAPSHOT_MOUNT_POINT="/mnt/snap"
@@ -14,7 +15,20 @@ UMOUNT=$(which umount)
 TAR=$(which tar)
 
 # Create the snapshot volume
-VOLUME_NAME="$VOLUME_GROUP/$LOGICAL_VOLUME"
-echo $VOLUME_NAME
-$LVCREATE -L $SNAPSHOT_SIZE -s -n $SNAPSHOT_NAME $VOLUME_NAME
+echo "Creating snapshot for volume $VOLUME_NAME"
+$LVCREATE -L $SNAPSHOT_SIZE -s -n $SNAPSHOT_NAME $VOLUME_NAME &> /dev/null
+if [ $? -ne 0 ];
+then
+	echo "Snapshot creation failed."
+	exit 1
+fi
+
+# Remove the snapshot
+echo "Removing snapshot for volume $VOLUME_NAME"
+$LVREMOVE -f $VOLUME_NAME &> /dev/null
+if [ $? -ne 0 ];
+then
+	echo "Removing snapshot failed."
+	exit 1
+fi
 
