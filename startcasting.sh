6df2cfd8c6ac95b1a9dd5071e86942aa2d9f5782
Marc Schlechter <marc@schlechter.nl>
2016-09-02 08:20:32 +0000

    updated

--- startcasting.sh
+++ startcasting.sh
@@ -4,6 +4,9 @@
 #
 # Zorg ervoor dat midori, unclutter en matchbox geinstalleerd zijn.
 #
+# Je kunt dit script installeren door het op te slaan als het ~/.xsession
+# bestand. Zodra X wordt opgestart, worden de onderstaande commando's
+# uitgevoerd.
 
 # Zet hier de URL
 URL=http://narrowcast.autotaal.biz/webplayer/imageviewer2.aspx?nodeid=417
@@ -14,6 +17,7 @@ xset -dpms # disable DPMS (Energy Star) features.
 xset s off # disable screen saver
 xset s noblank # don't blank the video device
 
+# Start midori fullscreen en verberg de mouse pointer
 matchbox-window-manager & unclutter & 
 midori -e Fullscreen -a $URL
 
